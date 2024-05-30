b5d1f26bd8da8065fb7a794c5784bfec77230c08
amandasuzzanne <amandasuzanne12@gmail.com>
2024-05-30 10:14:22 +0300

    Complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,9 @@
 
 def return_evens(num_list):
     pass
+    return[ number for number in num_list if number % 2 == 0]
 
 def make_exclamation(sentence_list):
     pass
+    return[f"{sentence}!" for sentence in sentence_list]
+
